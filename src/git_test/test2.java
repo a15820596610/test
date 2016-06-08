108c30c4a334d2bed07fafc91ebd314ca2686665
just <just@just-PC>
2016-06-09 01:25:36 +0800

    1.25

--- src/git_test/test2.java
+++ src/git_test/test2.java
@@ -3,8 +3,8 @@ package git_test;
 public class test2 {
 
 	public static void main(String[] args) {
-		System.out.println("b");
-		System.out.println("bbbbbbbbbbbb");
+		System.out.println("2");
+		System.out.println("2222222222");
 
 	}
 
