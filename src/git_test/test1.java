ac20f1fd67301fb9025f5d556fb4ce6bc3fe6415
just <just@just-PC>
2016-06-09 00:53:50 +0800

    部署 0.53

---
+++ src/git_test/test1.java
@@ -0,0 +1,10 @@
+package git_test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("1111111");
+		System.out.println("a111111111");
+	}
+
+}
---
+++ src/git_test/test2.java
@@ -0,0 +1,11 @@
+package git_test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("22222222");
+		System.out.println("b2222222222");
+
+	}
+
+}
