f3119f49c40144365648550379f7f3f6b7d308c3
xu <928782500@qq.com>
2016-06-09 01:07:17 +0800

    1.07

--- src/git_test/test1.java
+++ src/git_test/test1.java
@@ -3,8 +3,8 @@ package git_test;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("1111111");
-		System.out.println("a111111111");
+		System.out.println("a");
+		System.out.println("aaaaa");
 	}
 
 }
--- src/git_test/test2.java
+++ src/git_test/test2.java
@@ -3,8 +3,8 @@ package git_test;
 public class test2 {
 
 	public static void main(String[] args) {
-		System.out.println("22222222");
-		System.out.println("b2222222222");
+		System.out.println("b");
+		System.out.println("bbbbbbbbbbbb");
 
 	}
 
