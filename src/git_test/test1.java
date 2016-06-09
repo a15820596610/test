abaa0cb6b9aabf6ef7eb4a0c1be3278e22350eba
a15820596610 <2971146923@qq.com>
2016-06-09 14:56:53 +0800

    14.56

--- src/git_test/test1.java
+++ src/git_test/test1.java
@@ -2,7 +2,7 @@ package git_test;
 
 public class test1 {
 
-	public static void main(String[] args) {
+	public static 	void main(String[] args) {
 		System.out.println("a");
 		System.out.println("aaaaaaaaaa");
 	}
