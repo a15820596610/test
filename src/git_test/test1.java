5b751fdda727c71657972571f3ccd4e3dc739f41
a15820596610 <2971146923@qq.com>
2016-06-09 14:44:43 +0800

    test1 改成字母  14.44

--- src/git_test/test1.java
+++ src/git_test/test1.java
@@ -3,8 +3,8 @@ package git_test;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("111111111");
+		System.out.println("a");
+		System.out.println("aaaaaaaaaaa");
 	}
 
 }
